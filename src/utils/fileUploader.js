ae1561ba7c4a0af0a4ed950278748a0558fd7d57
abdo121212 <atfbdallh147@gmail.com>
2024-05-18 18:56:37 +0300

    update file filter

--- src/utils/fileUploader.js
+++ src/utils/fileUploader.js
@@ -17,7 +17,7 @@
 import multer, { diskStorage } from "multer";
 
 export const fliterObject = {
-  image: ["image/jpg", "image/png",  "image/jpeg"],
+  image: ["image/jpg", "image/png", "image/jpeg", "image/webp"],
   pdf: ["application/pdf"],
   video: ["video/mp4"],
 };
